74e92803975477044f2070ed5274a181e20888a8
anddiz <44113509+anddiz@users.noreply.github.com>
2019-10-11 08:49:46 +0200

    Create Use_Strings.py

---
+++ Use_Strings.py
@@ -0,0 +1,21 @@
+### A few functions built-in to the Python interpreter that work with strings
+## chr() converts an integer to a character
+## ord() converts a character to an integer
+## len() REturns the length of a string 
+## str() Returns a string representation of an object
+
+
+ord('a')
+ord(' ')
+ord('#')
+
+chr(97)
+chr(35)
+
+s = 'I am a string'
+len(s)
+
+
+str(49.2)
+str(3 + 29)
+
